d8e828aa813a1e9cb29a97f3781e9acb86f5da46
nabilbilly <billsonb05@gmail.com>
2026-02-04 12:04:30 +0000

    Fix cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,6 @@ from app.core.config import settings
 from app.db import base # noqa
 from app.db.session import get_db
 from app.api import api_router
-import json
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
@@ -23,8 +22,7 @@ origins = json.loads(settings.BACKEND_CORS_ORIGINS)
 # Set all CORS enabled origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    # allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+    allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
